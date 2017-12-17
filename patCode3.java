093a0fa019b4e39a1e25d6028dea99fcb1760c8f
unknown <patrick@gmail.com>
2017-12-17 11:35:05 +0800

    code3

---
+++ patCode3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = 0;
+		int oneCount = 0;
+		int zeroCount = 0;
+		
+		for (int i = 0; i < n; i++) {
+			String c = sc.next();
+			if (c.equals("0")) {
+				zeroCount++;
+			} else {
+				oneCount++;
+			}
+			if (zeroCount > oneCount) {
+				count += zeroCount;
+				zeroCount = oneCount = 0;
+			}
+		}
+		count += oneCount;
+		System.out.println(count);
+	}
+}
