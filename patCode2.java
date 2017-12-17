47f6c34ae0683e26fb30db87b447f188f6f231c4
unknown <patrick@gmail.com>
2017-12-17 11:34:57 +0800

    code2

---
+++ patCode2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Tom {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		ArrayList<String> a=new ArrayList<String>();
+		int n=scan.nextInt();
+	
+		for(int i=0;i<n;i++) {
+			String temp=scan.next();
+			if(a.contains(temp)){
+			System.out.println("YES");
+			}else {
+			System.out.println("NO");
+			a.add(temp);
+			}
+		}
+	}
+}
